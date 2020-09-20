307bbe1d71a2a5706ec9ccee570b5c3823a114a2
apolopena <penavilla@gmail.com>
2020-09-20 12:19:39 -0700

    got prism working

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,55 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Location } from '@reach/router'
 
+import { useEffect } from 'react'
+
+import Prism from 'prismjs'
+
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <Location>
-      {({ location }) => {
-        console.log(location)
-        return <p>The location is {location.pathname}</p>
-      }}
-    </Location>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import 'antd-mobile/dist/antd-mobile.css'
+import { Button } from 'antd-mobile'
+
+const sampleCode = `
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+`
+
+const IndexPage = () => {
+  useEffect(() => {
+    Prism.highlightAll()
+  })
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      <Location>
+        {({ location }) => {
+          console.log(location)
+          return <p>The location is {location.pathname}</p>
+        }}
+      </Location>
+      <Button>antd-mobile button</Button>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div className="code-container">
+        <pre>
+          <code className="language-jsx">{sampleCode}</code>
+        </pre>
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
